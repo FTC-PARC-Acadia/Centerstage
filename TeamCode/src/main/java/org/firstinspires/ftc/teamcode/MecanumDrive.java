16495dcc0c7271134e7ab7d12543d0503d554a97
Chemi <abbyandam@gmail.com>
2022-09-18 15:41:30 -0400

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -6,10 +6,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
 public class MecanumDrive {
-    private DcMotor frontLeftDrive;
-    private DcMotor frontRightDrive;
-    private DcMotor backLeftDrive;
-    private DcMotor backRightDrive;
+    private final DcMotor frontLeftDrive;
+    private final DcMotor frontRightDrive;
+    private final DcMotor backLeftDrive;
+    private final DcMotor backRightDrive;
 
     private double frontRightBackLeftPower;
     private double frontLeftBackRightPower;
@@ -21,7 +21,7 @@ public class MecanumDrive {
         this.backRightDrive = backRightDrive;
     }
 
-    private void robotCentric() {
+    public void robotCentric() {
         frontRightBackLeftPower = Range.clip(Math.sin(Math.atan(gamepad1.left_stick_y/gamepad1.left_stick_x)-0.25*Math.PI)*Math.sqrt(Math.pow(gamepad1.left_stick_x, 2) + Math.pow(gamepad1.left_stick_y, 2)), -1.0, 1.0);
         frontLeftBackRightPower = Range.clip(Math.sin(Math.atan(gamepad1.left_stick_y/gamepad1.left_stick_x)+0.25*Math.PI)*Math.sqrt(Math.pow(gamepad1.left_stick_x, 2) + Math.pow(gamepad1.left_stick_y, 2)), -1.0, 1.0);
 
