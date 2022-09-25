94dd244f1bc39065f0f09ac26f68adc230a0fd5a
AnnoyingDoge <60070276+AnnoyingDoge@users.noreply.github.com>
2022-09-25 15:32:23 -0400

    Refactored Mecanum + started robot centric

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearOpMode.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,7 +12,7 @@ public class LinearOpMode extends OpMode {
     MecanumDrive drive;
 
     public void init() {
-        drive = new MecanumDrive(hardwareMap.get(DcMotor.class, "frontLeftDrive"), hardwareMap.get(DcMotor.class, "frontRightDrive"), hardwareMap.get(DcMotor.class, "backLeftDrive"), hardwareMap.get(DcMotor.class, "backRightDrive"), gamepad1);
+        drive = new MecanumDrive(hardwareMap.get(DcMotor.class, "frontLeftDrive"), hardwareMap.get(DcMotor.class, "frontRightDrive"), hardwareMap.get(DcMotor.class, "backLeftDrive"), hardwareMap.get(DcMotor.class, "backRightDrive"), gamepad1, hardwareMap.get(BNO055IMU.class, "imu"));
     }
 
     public void loop() {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 public class MecanumDrive {
     //Motor Variables
@@ -10,7 +15,7 @@ public class MecanumDrive {
     private DcMotor frontRightDrive;
     private DcMotor backLeftDrive;
     private DcMotor backRightDrive;
-    
+
     private Gamepad gamepad1;
 
     //Power Variables
@@ -19,15 +24,20 @@ public class MecanumDrive {
     private double backLeftPower;
     private double backRightPower;
 
+    //IMU Variables
+    BNO055IMU imu;
+    Orientation orientation;
 
-
-    public MecanumDrive(DcMotor frontLeftDrive, DcMotor frontRightDrive, DcMotor backLeftDrive, DcMotor backRightDrive, Gamepad gamepad1) {
+    public MecanumDrive(DcMotor frontLeftDrive, DcMotor frontRightDrive, DcMotor backLeftDrive, DcMotor backRightDrive, Gamepad gamepad1, BNO055IMU imu) {
         this.frontLeftDrive = frontLeftDrive;
         this.frontRightDrive = frontRightDrive;
         this.backLeftDrive = backLeftDrive;
         this.backRightDrive = backRightDrive;
 
         this.gamepad1 = gamepad1;
+
+        //Should pass this through from main class.
+        this.imu = imu;
     }
 
     public void robotCentricDrive() {
@@ -40,6 +50,32 @@ public class MecanumDrive {
         joystickMagnitude = Math.sqrt(Math.pow(gamepad1.left_stick_y, 2) + Math.pow(gamepad1.left_stick_x, 2));
         turn = gamepad1.right_stick_x;
 
+        drive(joystickAngle, joystickMagnitude, turn);
+    }
+
+    public void fieldCentricDrive() {
+        //I think we just add angle to the joystickAngle of robotCentricDrive
+        double joystickAngle;
+        double joystickMagnitude;
+        double turn;
+        double newAngle;
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        imu.initialize(parameters);
+
+        //Calculating magnitude of joystick vector
+        joystickAngle = gamepad1.left_stick_x < 0 ? Math.atan(gamepad1.left_stick_y/gamepad1.left_stick_x) + Math.PI : Math.atan(gamepad1.left_stick_y/gamepad1.left_stick_x);
+        joystickMagnitude = Math.sqrt(Math.pow(gamepad1.left_stick_y, 2) + Math.pow(gamepad1.left_stick_x, 2));
+        turn = gamepad1.right_stick_x;
+
+
+
+        drive(joystickAngle, joystickMagnitude, turn);
+    }
+
+    public void drive(double joystickAngle, double joystickMagnitude, double turn)
+    {
         //Mecanum math, joystick angle and magnitude --> motor power
         frontLeftPower = Range.clip((Math.sin(joystickAngle) * joystickMagnitude) - Math.cos(joystickAngle) * joystickMagnitude, -1, 1);
         backRightPower = Range.clip(-(Math.sin(joystickAngle) * joystickMagnitude - Math.cos(joystickAngle) * joystickMagnitude + turn), -1, 1);
@@ -52,9 +88,4 @@ public class MecanumDrive {
         backLeftDrive.setPower(backLeftPower);
         backRightDrive.setPower(backRightPower);
     }
-
-    public void fieldCentricDrive() {
-
-    }
-
 }
