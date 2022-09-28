ed00eec3d82d3696e56c88fd25adc19dbb6dd6c9
AnnoyingDoge <60070276+AnnoyingDoge@users.noreply.github.com>
2022-09-28 08:58:08 -0400

    new opmode for mecanum
    i just made a new one thats actually called mecanumopmode cuz why not i guess. it should work

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumOpMode.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "MecanumOpMode", group = "TeleOpModes")
+public class MecanumOpMode extends OpMode {
+    MecanumDrive drive;
+
+    public void init() {
+        drive = new MecanumDrive(hardwareMap.get(DcMotor.class, "frontLeftDrive"), hardwareMap.get(DcMotor.class, "frontRightDrive"), hardwareMap.get(DcMotor.class, "backLeftDrive"), hardwareMap.get(DcMotor.class, "backRightDrive"), gamepad1, hardwareMap.get(BNO055IMU.class, "imu"));
+    }
+
+    public void loop() {
+        drive.robotCentricDrive();
+    }
+}
