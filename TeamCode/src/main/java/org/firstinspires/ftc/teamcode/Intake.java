e5744805a9cc158bc2d243b3be199ab82ca7d188
Chemi <abbyandam@gmail.com>
2022-09-28 19:09:39 -0400

    Create Intake.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Intake extends MecanumDrive {
+
+    private Gamepad gamepad2;
+    private DcMotor lift;
+    private Servo servo;
+    private int position;
+
+    public Intake(DcMotor frontLeftDrive, DcMotor frontRightDrive, DcMotor backLeftDrive, DcMotor backRightDrive, DcMotor lift, Servo servo, Gamepad gamepad1, Gamepad gamepad2, BNO055IMU imu) {
+        super(frontLeftDrive, frontRightDrive, backLeftDrive, backRightDrive, gamepad1, imu);
+
+        this.lift = lift;
+        this.servo = servo;
+        this.gamepad2 = gamepad2;
+
+        position = 5;
+    }
+
+    public void lift() {
+        lift.setTargetPosition(position);
+        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        if (gamepad2.right_bumper && lift.isBusy()) {
+            lift.setPower(.5);
+        } else if (lift.isBusy()) {
+            lift.setPower(-.5);
+        } else {
+            position = -position;
+        }
+    }
+
+    public void intake() {
+
+    }
+}
