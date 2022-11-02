a42f0dd7231112a99afda1038a147139a426326f
Chemi <abbyandam@gmail.com>
2022-11-02 18:38:28 -0400

    changed code structure

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -10,7 +10,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-public class MecanumDrive {
+public class MecanumDrive extends Robot {
     //Motor Variables
     private DcMotor frontLeftDrive;
     private DcMotor frontRightDrive;
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Robot implements Intake2, Lift {
+}
