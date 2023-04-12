5da041d56cbbbc8876ef6e2b8d788850695f57ff
sbarton237 <72349665+sbarton237@users.noreply.github.com>
2023-04-12 19:34:05 -0400

    Update TeleOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -11,7 +11,11 @@ public class TeleOpMode extends OpMode {
     Robot robot;
 
     public void init() {
-        robot = new Robot(gamepad1, gamepad2, hardwareMap.get(Servo.class, "intake"), hardwareMap.get(DcMotor.class, "lift"), hardwareMap.get(DcMotor.class, "frontLeftDrive"), hardwareMap.get(DcMotor.class, "frontRightDrive"), hardwareMap.get(DcMotor.class, "backLeftDrive"), hardwareMap.get(DcMotor.class, "backRightDrive"), hardwareMap.get(BNO055IMU.class, "imu"));
+        robot = new Robot(gamepad1, gamepad2, hardwareMap.get(Servo.class, "intake"),
+                hardwareMap.get(DcMotor.class, "lift"), hardwareMap.get(DcMotor.class,
+                "frontLeftDrive"), hardwareMap.get(DcMotor.class, "frontRightDrive"),
+                hardwareMap.get(DcMotor.class, "backLeftDrive"), hardwareMap.get(DcMotor.class,
+                "backRightDrive"), hardwareMap.get(BNO055IMU.class, "imu"));
     }
 
     public void loop() {
