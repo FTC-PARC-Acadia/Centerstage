cd9b7ecb7eb50f97f2083ac6e7d1b5ac80a012c2
sbarton237 <72349665+sbarton237@users.noreply.github.com>
2023-10-11 14:29:09 -0400

    added intake

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Intake {
+
+    public DcMotor intake;
+    public Gamepad gamepad2;
+    public HardwareMap hardwareMap;
+
+    private double power = 0.5;
+    private boolean stopped = false;
+
+    public Intake(Gamepad gamepad2, DcMotor intake) {
+        this.gamepad2 = gamepad2;
+        this.intake = intake;
+    }
+
+    public Intake(DcMotor intake) {
+        this.intake = intake;
+    }
+
+    public void stop() {
+        if (gamepad2.a && !stopped) {
+            intake.setPower(0);
+            stopped = true;
+        }
+        else if (gamepad2.a && stopped) {
+            intake.setPower(power);
+            stopped = false;
+        }
+    }
+
+    public void flip() {
+        if (gamepad2.y && !stopped) {
+            power = - power;
+            intake.setPower(power);
+        }
+    }
+
+}
